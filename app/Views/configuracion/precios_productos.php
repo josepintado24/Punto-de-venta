b67436f376f90b826960abe6597fec5d785c932c
AngelAbelPelaye <angelabelpelaye@gmail.com>
2020-12-30 12:58:56 -0300

    Actualización de Secciónes

--- app/Views/configuracion/precios_productos.php
+++ app/Views/configuracion/precios_productos.php
@@ -15,11 +15,23 @@
 
                                     <?php  
                                     foreach ($datos as $dato){?>
-                                                    <div class="col-12 col-sm-6">
-                                            <label for="">Nombre corto: <?php echo $dato['nombre']; ?></label>
-                                            <label for="">precio Venta: </label><input value="<?php echo $dato['precio_venta'];?> " class="form-control" id="nombre_corto" name="nombre_corto" typr="text" >
-                                            <label for= "">precio compra:</label><input value="<?php echo $dato['precio_compra'];?> " class="form-control" id="nombre_corto" name="nombre_corto" typr="text" >
-                                        </div>
+
+
+                                        <table class="table">
+                                            <thead class="thead-dark">
+                                              <tr>
+                                                <th scope="col">Nombre Corto</th>
+                                                <th scope="col">Precio Compra</th>
+                                                <th scope="col">Precio Venta</th>
+                                              </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                  <td><?php echo $dato['nombre']; ?></td>
+                                                  <td><input value="<?php echo $dato['precio_venta'];?> " class="form-control" id="nombre_corto" name="nombre_corto" typr="text" ></td>
+                                                  <td><input value="<?php echo $dato['precio_compra'];?> " class="form-control" id="nombre_corto" name="nombre_corto" typr="text" ></td>
+                                                </tr>
+                                        </table>
                                                        
 
                                          <?php }
@@ -28,8 +40,22 @@
                                         
                                     </div>    
                                 </div>
-                                    <a href="<?php //echo base_url();?>/configuracion" class="btn btn-primary">Regresar</a>
-                                    <button type="submit" class="btn btn-success">Guardar </button>
+
+                                <!--**___BOTONES REGRESAR Y GUARDAR___**-->
+
+                                <a href="<?php //echo base_url();?>/configuracion" class="btn btn-info mt-3">
+                                    <svg class="mr-2" width="23" height="19" viewBox="0 0 23 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M8.3125 18.4375L0.5 10.625L0.5 8.4375L8.3125 0.624999L10.5313 2.8125L5.40625 7.96875L22.8125 7.96875L22.8125 11.0938L5.40625 11.0937L10.5625 16.25L8.3125 18.4375Z" fill="#E0E0E0"/>
+                                    </svg>      
+                                Regresar
+                                </a>
+
+                                <button type="submit" class="btn btn-success float-right mt-3">
+                                    <svg class="mr-2" width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M16.2239 0L19.5999 3.376L7.1519 15.84L0.399902 9.08L3.7759 5.704L7.1519 9.08L16.2239 0ZM16.2239 2.24L7.1519 11.328L3.7759 7.992L2.6479 9.08L7.1519 13.576L17.3519 3.376L16.2239 2.24Z" fill="white"/>
+                                    </svg>
+                                    Guardar 
+                                </button>
                                
                                 
                             </form>
