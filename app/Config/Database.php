c26f4dc470a0a3cb0fb562a9122732f4e6b28bcc
Jose Luis Pintado Vasquez <i201716389@cibertec.edu.pe>
2021-09-05 02:32:47 -0500

    configurar git ignore

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends \CodeIgniter\Database\Config
 		'hostname' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => 'maximiliano_tienda',
+		'database' => 'pos',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
